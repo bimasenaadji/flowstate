437088c13b13a8e2624563d04b17d73764c61516
bimasenaadji <bimasenaadji@gmail.com>
2026-03-14 19:04:04 +0700

    feat: refactor TransactionForm to include type definitions and handle form submission

--- src/components/dashboard/TransactionForm.tsx
+++ src/components/dashboard/TransactionForm.tsx
@@ -1,8 +1,49 @@
-export default function TransactionForm({ className }: { className?: string }) {
+import type { Transaction } from "../../types"; // Sesuaikan path-mu
+
+interface TransactionFormProps {
+  className?: string;
+
+  onAddTransaction: (data: Transaction) => void;
+}
+
+export default function TransactionForm({
+  className,
+  onAddTransaction,
+}: TransactionFormProps) {
+  const handleLocalSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const name = formData.get("transaction-name") as string;
+    const amount = Number(formData.get("amount"));
+    const type = formData.get("income-type") as
+      | "income"
+      | "expense"
+      | "installment";
+
+    const newTrx: Transaction = {
+      id: `trx-${Date.now()}`,
+      name,
+      amount,
+      type,
+      date: new Date().toLocaleDateString("id-ID", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+      }),
+    };
+
+    onAddTransaction(newTrx);
+
+    e.currentTarget.reset();
+  };
+
   return (
     <div className={`${className} p-5 bg-card rounded-md shadow-sm`}>
       <h3 className="text-xl font-semibold mb-3">Add Transaction</h3>
-      <form className="space-y-2">
+      {/* 4. Arahkan onSubmit ke Pawang Form lokal */}
+      <form className="space-y-2" onSubmit={handleLocalSubmit}>
         <div className="flex flex-col space-y-4">
           <label className="font-semibold text-sm" htmlFor="transaction-name">
             Transaction Name
